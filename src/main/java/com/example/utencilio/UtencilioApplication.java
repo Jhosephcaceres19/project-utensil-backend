0ca38adf6ddbacb77450525ffcfd462104308781
Edmilzon <edmilzon.luna@gmail.com>
2025-04-30 00:37:22 -0400

    first commit

---
+++ src/main/java/com/example/utencilio/UtencilioApplication.java
@@ -0,0 +1,13 @@
+package com.example.utencilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtencilioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtencilioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/utencilio/UtencilioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.utencilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtencilioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
