333472d641a154060ef6f9c3cf9caac4fdb13fb9
Edmilzon <edmilzon.luna@gmail.com>
2025-05-03 17:19:22 -0400

    Insertar conf inicial

---
+++ src/main/java/com/example/Auth/AuthController.java
@@ -0,0 +1,24 @@
+package com.example.Auth;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    
+    @PostMapping(value = "login")
+    public String login (){
+        return "hola desde login";
+    }
+
+    @PostMapping(value = "register")
+    public String register(){
+        return "Hola desde register";
+    }
+}
+
